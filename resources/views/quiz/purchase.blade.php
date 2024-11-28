c81cd111c5dc915169163f699408bfefdf9c9315
asanoopsharm3 <asanoopsharma03@gmail.com>
2024-11-28 09:53:14 -0800

    make build

--- resources/views/quiz/purchase.blade.php
+++ resources/views/quiz/purchase.blade.php
@@ -4,32 +4,71 @@
 <main>
     <div class="container">
     @include('header')
-    <div class="plan-info">
-        <div>
-        
-        <img src="{{ asset('images/plan.svg') }}">
-    </div>
-    <div>
-        <h3>IT’S GREAT <br>
-            TO HAVE </h3>
-           <h3 class="gri-text"> YOU! </h3>
-            <p>Please select the play  pack to
-                proceed</p>
-            </div>
-     </div>
-    <div class="row">
-        @foreach($plans as $key=>$plan)
+
+        <!-- @foreach($plans as $key=>$plan)
         @php
             $planImage = $planImagesArr[$key];
         @endphp
         <div class="col-md-4">
-            <img src="<?php echo asset('images/'.$planImage) ?>"/>
+            <img src="<?php //echo asset('images/'.$planImage) ?>"/>
             <h3>{{ ucfirst($plan->duration) }} @ {{ $plan->price }} SZL</h3>
-            <button onclick="selectPlan('<?php echo $plan->id; ?>');"><span>cLICK</span></button>
+            <button onclick="selectPlan('<?php //echo $plan->id; ?>');"><span>cLICK</span></button>
         </div>
-        @endforeach
-     </div>
+        @endforeach -->
+        </div>
+        <div class="sec-2">
+        <div class="container">
+        <div class="row Products-option">
+           <div class="col-md-4 basic">
+               <img src="<?php echo asset('images/basic.png') ?>">
+               <h3>Basic Plan</h3>
+               <h1>$9.9/ month</h1>
+              
+               <h3>Featured Included:</h3>
+               <ul>
+                <li>24/7 Network Monitoring</li>
+                <li>Helpdesk Support (Limited Hours)</li>
+                <li>Basic Cybersecurity Protection
+                </li>
+                <li>24/7 Network Monitoring</li>
+               </ul>
+               <div class="btn">Buy Now <i class="fa fa-long-arrow-right" aria-hidden="true"></i></div>
+           </div>
+           <div class="col-md-4 sliver">
+            <img src="<?php echo asset('images/crystal.webp') ?>">
+            <h3>Sliver Plan</h3>
+            <h1>$9.9/ month</h1>
+            
+            <h3>Featured Included:</h3>
+            <ul>
+             <li>24/7 Network Monitoring</li>
+             <li>Helpdesk Support (Limited Hours)</li>
+             <li>Basic Cybersecurity Protection
+             </li>
+             <li>24/7 Network Monitoring</li>
+            </ul>
+            <div class="btn">Buy Now <i class="fa fa-long-arrow-right" aria-hidden="true"></i></div>
+        </div>
+        <div class="col-md-4 premium">
+            <img src="<?php echo asset('images/diamond-svgrepo-com.svg') ?>">
+            <h3>Premium Plan</h3>
+            <h1>$9.9/ month</h1>
+           
+            <h3>Featured Included:</h3>
+            <ul>
+             <li>24/7 Network Monitoring</li>
+             <li>Helpdesk Support (Limited Hours)</li>
+             <li>Basic Cybersecurity Protection
+             </li>
+             <li>24/7 Network Monitoring</li>
+            </ul>
+            <div class="btn">Buy Now <i class="fa fa-long-arrow-right" aria-hidden="true"></i></div>
+        </div>
+        </div>
     </div>
+      
+    
+   
 </main>
 <script>
         function selectPlan(planId){
